4250a0cbd3559c8e16de38f41c0ff5a19f1f7d31
janmatzek <jan.matzek@gmail.com>
2025-10-13 16:38:27 +0200

    refactor: remove redundant tests

--- tests/fake_aws_creds.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# This creates fake ~/.aws/credentials file for boto3 mocking
-mkdir ~/.aws && touch ~/.aws/credentials && echo "[default]\naws_access_key_id = test\naws_secret_access_key = test" > ~/.aws/credentials
